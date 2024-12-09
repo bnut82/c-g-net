76a785f46995c8c65a0dbf35b2e514dd045f6abd
Jakub Bartkiewicz <bartkiewiczj@gmail.com>
2024-12-09 11:13:20 +0100

    remove extra line

--- Challenge.Contracts/SearchArg.cs
+++ Challenge.Contracts/SearchArg.cs
@@ -4,6 +4,5 @@ public class SearchArg
 {
     public required string HotelName { get; set; }
     public required string RoomType { get; init; }
-
     public required SearchArgDate Dates { get; set; }
 }
